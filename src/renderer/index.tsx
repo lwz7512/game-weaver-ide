b29560642c616e706a0dc8bd1df272986d86f720
liwenzhi <lwz7512@gmail.com>
2022-08-16 09:49:23 -0300

    first commit

---
+++ src/renderer/index.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+const rootApp = createRoot(container || document.body);
+rootApp.render(<App />);
+
+// calling IPC exposed from preload script
+window.electron.ipcRenderer.once('ipc-example', (arg) => {
+  // eslint-disable-next-line no-console
+  console.log(arg);
+});
+window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
